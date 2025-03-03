76b2a949bd72d1c76e6f76090b1636d8d4fb1586
Hans-Joachim Belz <hans-joachim.belz@anstrengungslos.de>
2025-03-03 17:30:16 +0100

    Added new models. Some prompt optimizations for step 07.

--- runAllModelsForPath.sh
+++ runAllModelsForPath.sh
@@ -12,17 +12,19 @@ fi
 # GPT by OpenAI
 ./evalDatePrompts.sh gpt-4o "$promptPath"
 ./evalDatePrompts.sh gpt-4o-mini "$promptPath"
+./evalDatePrompts.sh gpt-4.5-preview "$promptPath"
 # ./evalDatePrompts.sh o3-mini "$promptPath"
 
 # Claude by Anthropic
 ./evalDatePrompts.sh claude-3.5-sonnet "$promptPath"
+./evalDatePrompts.sh claude-3.7-sonnet "$promptPath"
 ./evalDatePrompts.sh claude-3.5-haiku "$promptPath"
 
 # Google Gemini
 # ./evalDatePrompts.sh gemini-1.5-pro-latest "$promptPath"
 # ./evalDatePrompts.sh gemini-2.0-pro-exp-02-05 "$promptPath"
 ./evalDatePrompts.sh gemini-2.0-flash "$promptPath"
-./evalDatePrompts.sh gemini-2.0-flash-lite-preview-02-05 "$promptPath"
+./evalDatePrompts.sh gemini-2.0-flash-lite "$promptPath"
 # ./evalDatePrompts.sh gemini-2.0-flash-thinking-exp-01-21 "$promptPath"
 
 # Llama by Meta (localvia Ollama)
